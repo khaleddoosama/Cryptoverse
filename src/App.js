a015add92d9d586bf307e2e4710d20b0cde85a0b
khaleddoosama <khaleddoosama@gmail.com>
2024-12-22 04:52:24 +0200

    for deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Link, Routes } from 'react-router-dom';
+
+import { Typography, Space } from 'antd';
+
+import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
     </div>
-  );
-}
+    <div className="main">
+        <div className="routes">
+          <Routes>
+            <Route  path="/" element={(<Homepage />)} />
+            <Route  path="/exchanges" element={(<Exchanges />)} />
+            <Route  path="/cryptocurrencies" element={(<Cryptocurrencies />)} />
+            <Route  path="/crypto/:coinId" element={(<CryptoDetails />)} />
+            <Route  path="/news" element={(<News />)} />
+          </Routes>
+        </div>
+      <div className="footer" >
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+          Cryptoverse <br />
+          All right reserverd
+        </Typography.Title>
+        <Space>
+          <Link to='/'>Home</Link>
+          <Link to='/exchanges'>Exchanges</Link>
+          <Link to='/news'>News</Link>
+        </Space>
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
+
+/* <Route exact path="/" element={(<Homepage />)}>
+              <Homepage />
+            </Route>
+            <Route exact path="/exchanges"> 
+              <Exchanges />
+            </Route>
+            <Route exact path="/cryptocurrencies">
+              <Cryptocurrencies />
+            </Route>
+            <Route exact path="/crypto/:coinId">
+              <CryptoDetails />
+            </Route>
+            <Route exact path="/news">
+              <News />
+            </Route>*/
